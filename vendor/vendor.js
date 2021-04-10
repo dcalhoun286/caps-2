271371ecaca30e0ba6708f131550c6453541982a
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2021-04-09 18:41:46 -0700

    required faker; renamed namespace; added customer order

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -2,14 +2,32 @@
 
 require('dotenv').config();
 
+const faker = require('faker');
+
 const io = require('socket.io-client');
 
 const PORT = process.env.PORT;
 
 const host = `http://localhost:${PORT}`;
 
-const socket = io.connect(`${host}/deliveries`);
+const caps = io.connect(`${host}/caps`);
 
 function timestamp() {
   return new Date().toDateString();
 }
+
+setInterval(() => {
+  let storeName = '1-206-flowers';
+  let storeId = process.env.STOREID;
+  let customerName = faker.name.findName();
+  let shippingAddress = `${faker.address.streetAddress('###')}, ${faker.address.city()}, ${faker.address.stateAbbr()} ${faker.address.zipCode('#####')}`;
+
+  let payload = {
+    storeName: storeName,
+    storeId: storeId,
+    customerName: customerName,
+    shippingAddress: shippingAddress
+  }
+
+  caps.emit('pickup', payload)
+}, 5000);
